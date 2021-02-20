cab7cdcd6b94d61284fdf9b2ae551d996f493ad5
Irelia <yaokai729@outlook.com>
2021-02-20 23:15:26 +0800

    下载next-images插件，处理图片文件

--- next.config.js
+++ next.config.js
@@ -1,20 +1,27 @@
-module.exports = {
-  webpack: (config, options) => {
-    const isServer = options.isServer
-    config.module.rules.push({
-      test: /\.(png|jpg|jpeg|gif|svg)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[contenthash].[ext]', // 文件名 + 哈希
-            outputPath: 'static', // 硬盘路径
-            publicPath: '_next/static'  // 网站路径
-          }
-        },
-      ],
-    })
+// module.exports = {
+//   webpack: (config, options) => {
+//     const isServer = options.isServer
+//     config.module.rules.push({
+//       test: /\.(png|jpg|jpeg|gif|svg)$/,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {
+//             name: '[name].[contenthash].[ext]', // 文件名 + 哈希
+//             outputPath: 'static', // 硬盘路径
+//             publicPath: '_next/static'  // 网站路径
+//           }
+//         },
+//       ],
+//     })
 
+//     return config
+//   },
+// }
+
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
     return config
-  },
-}
+  }
+})
