3730a02d47b1c96ef5c2b5a95432da872025ecdc
Irelia <yaokai729@outlook.com>
2021-02-21 22:56:42 +0800

    新建hooks目录，封装 hooks

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,27 +1,8 @@
 import { NextPage } from "next"
-import axios from 'axios'
-import { useEffect, useState } from "react"
-
-type Post = {
-  id: string;
-  date: string;
-  title: string
-}
+import { usePosts } from "hooks/usePosts"
 
 const PostsIndex: NextPage = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(false)
-  useEffect(()=>{
-    setIsLoading(true)
-    axios.get('/api/v1/posts').then(response => {
-      setPosts(response.data)
-      setIsLoading(false)
-      if(response.data.length === 0){
-        setIsEmpty(true)
-      }
-    })
-  },[])
+  const {posts, isLoading, isEmpty} = usePosts()
   return (
     <div>
       <h1>文章列表</h1>
