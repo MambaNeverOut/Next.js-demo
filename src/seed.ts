e93b15b6d6d299440f118a80f495b7c6a9f7e497
Irelia <yaokai729@outlook.com>
2021-03-08 23:41:43 +0800

    使用manager操作实体，使用seed填充数据

---
+++ src/seed.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Post } from "./entity/Post";
+
+createConnection().then(async connection => {
+  const posts = await connection.manager.find(Post);
+  if (posts.length === 0) {
+    // const p = new Post('Post', '我的第一篇文章');
+    // p.title = 'Post 1';
+    // p.content = '我的第一篇文章';
+
+    await connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(n => {
+      return new Post({ title: `Post${n}`, content: `这是我的第${n}篇文章` })
+    }))
+    console.log('posts 数据填充了');
+
+  }
+
+  // const posts2 = await connection.manager.find(Post);
+  // console.log(posts2);
+  connection.close()
+}).catch(error => console.log(error));
