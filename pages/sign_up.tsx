e662af760837052af4aec379e8ffd14f6326144f
Irelia <yaokai729@outlook.com>
2021-03-20 23:17:06 +0800

    添加注冊页面，不过没有检查username唯一性

---
+++ pages/sign_up.tsx
@@ -0,0 +1,89 @@
+import axios, { AxiosResponse } from 'axios';
+import { NextPage } from 'next';
+import {useCallback, useState} from 'react';
+
+const SignUp: NextPage = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    passwordConfirmation: '',
+  })
+  const [errors, setErrors] = useState({
+    // 这里使用数组是为了方便类型判断
+    username: [], password: [], passwordConfirmation: []
+  })
+  const onSubmit = useCallback((e)=>{
+    e.preventDefault()
+    console.log(formData);
+    axios.post('api/v1/users', formData)
+    .then(()=>{}, (error)=>{
+      if(error.response){
+        const response: AxiosResponse = error.response;
+        if(response.status === 422){
+          console.log(errors);
+          console.log(response);
+          
+          
+          setErrors({...errors, ...response.data})
+          console.log(errors);
+          
+        }
+        console.log(error);
+      }
+      
+    })
+  }, [formData])
+  return (
+    <>
+      <h1>注册</h1>
+      {JSON.stringify(formData)}
+      <form onSubmit={onSubmit}>
+        {JSON.stringify(errors)}
+        <div>
+          <label>
+            用户名
+            <input type="text" value={formData.username}
+              onChange={e => setFormData({
+                ...formData,
+                username: e.target.value
+              })}
+            />
+          </label>
+          {errors.username && errors.username.length > 0 ? <div>
+            {errors.username.join(',')}
+          </div>: null}
+        </div>
+        <div>
+          <label>
+            密码
+            <input type="password" value={formData.password}
+            onChange={e => setFormData({
+              ...formData,
+              password:e.target.value
+            })}/>
+          </label>
+          {errors.password && errors.password.length > 0 ? <div>
+            {errors.password.join(',')}
+          </div>: null}
+        </div>
+        <div>
+          <label>
+            确认密码
+            <input type="password" value={formData.passwordConfirmation}
+              onChange={ e=> setFormData({
+                ...formData,
+                passwordConfirmation: e.target.value
+              })}
+            />
+          </label>
+          {errors.passwordConfirmation && errors.passwordConfirmation.length > 0 ? <div>
+            {errors.passwordConfirmation.join(',')}
+          </div>: null}
+        </div>
+        <button type="submit">注册</button>
+      </form>
+    </>
+  )
+}
+
+export default SignUp
